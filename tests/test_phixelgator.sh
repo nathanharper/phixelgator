27937c3fa2196b278b6a2b547a701cd8e3f5ee68
nathanharper <nathan.sam.harper@gmail.com>
2013-04-04 21:20:44 -0400

    removed flashman palette, as its a duplicate of the tetris palette

--- tests/test_phixelgator.sh
+++ tests/test_phixelgator.sh
@@ -3,7 +3,6 @@
 ../phixelgator.py -p atari2600 cat.jpg atari2600.png
 ../phixelgator.py -p commodore64 cat.jpg commodore64.png
 ../phixelgator.py -p contra cat.jpg contra.png
-../phixelgator.py -p flashman cat.jpg flashman.png
 ../phixelgator.py -p gameboy cat.jpg gameboy.png
 ../phixelgator.py -p grayscale cat.jpg grayscale.png
 ../phixelgator.py -p hyrule cat.jpg hyrule.png
