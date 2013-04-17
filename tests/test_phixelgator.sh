60ccf319766c2e6faee2b62bd5147a1e583c8b79
Nathan Harper <nathan.sam.harper@gmail.com>
2013-04-17 09:39:47 -0400

    changed block size to 4 in sample images

--- tests/test_phixelgator.sh
+++ tests/test_phixelgator.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-../phixelgator.py -p appleii ernie.jpg appleii.png
-# ../phixelgator.py -p atari2600 ernie.jpg atari2600.png
-../phixelgator.py -p commodore64 ernie.jpg commodore64.png
-../phixelgator.py -p contra ernie.jpg contra.png
-../phixelgator.py -p gameboy ernie.jpg gameboy.png
-../phixelgator.py -p grayscale ernie.jpg grayscale.png
-../phixelgator.py -p hyrule ernie.jpg hyrule.png
-../phixelgator.py -p intellivision ernie.jpg intellivision.png
-../phixelgator.py -p kungfu ernie.jpg kungfu.png
-../phixelgator.py -p mario ernie.jpg mario.png
-../phixelgator.py -p nes ernie.jpg nes.png
-../phixelgator.py -p sega ernie.jpg sega.png
-../phixelgator.py -p tetris ernie.jpg tetris.png
+../phixelgator.py -b4 -p appleii ernie.jpg appleii.png
+../phixelgator.py -b4 -p atari2600 ernie.jpg atari2600.png
+../phixelgator.py -b4 -p commodore64 ernie.jpg commodore64.png
+../phixelgator.py -b4 -p contra ernie.jpg contra.png
+../phixelgator.py -b4 -p gameboy ernie.jpg gameboy.png
+../phixelgator.py -b4 -p grayscale ernie.jpg grayscale.png
+../phixelgator.py -b4 -p hyrule ernie.jpg hyrule.png
+../phixelgator.py -b4 -p intellivision ernie.jpg intellivision.png
+../phixelgator.py -b4 -p kungfu ernie.jpg kungfu.png
+../phixelgator.py -b4 -p mario ernie.jpg mario.png
+../phixelgator.py -b4 -p nes ernie.jpg nes.png
+../phixelgator.py -b4 -p sega ernie.jpg sega.png
+../phixelgator.py -b4 -p tetris ernie.jpg tetris.png
